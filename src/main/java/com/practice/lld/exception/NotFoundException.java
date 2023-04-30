14cc774993c20e428cb33a02f304719ce0781326
dheeraj.mishra <dheeraj.mishra@khoros.com>
2023-04-30 16:24:35 +0530

    contact lld like truecaller

--- src/main/java/com/practice/lld/exception/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.practice.lld.exception;
-
-public class NotFoundException extends Throwable {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/com/practice/lld/search/utils/SearchUtils.java
+++
@@ -1,9 +0,0 @@
-package com.practice.lld.search.utils;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class SearchUtils {
-    public int MAX_SEARCH = 6;
-
-}
